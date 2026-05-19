9485446dd1447c096cc2648da001bec597d91ef8
mahaut231 <mg@mahaut.net>
2026-05-19 19:03:39 -0400

    modification et nettoyage de nettoyage.py

--- nettoyage.py
+++ nettoyage.py
@@ -1,14 +1,10 @@
-
 import os
 import shutil
 from sklearn.model_selection import train_test_split
 
-# ======================
-# PARAMÈTRES
-# ======================
-
-SOURCE_DATASET = "dataset\Plant_leave_diseases_dataset_without_augmentation"   # dossier original
-DESTINATION = "PlantVillage_split" # dossier de sortie
+#definition des paramètres
+SOURCE_DATASET = "dataset/Plant_leave_diseases_dataset_without_augmentation"
+DESTINATION = "PlantVillage_split"
 
 TRAIN_RATIO = 0.70
 VAL_RATIO = 0.15
@@ -16,16 +12,18 @@ TEST_RATIO = 0.15
 
 SEED = 42
 
-# ======================
-# CRÉATION DOSSIERS
-# ======================
+#reset au cas où
+
+if os.path.exists(DESTINATION):
+    print(f"️ Dossier existant supprimé : {DESTINATION}")
+    shutil.rmtree(DESTINATION)
+
+#creation des sous dossiers
 
 for subset in ["train", "validation", "test"]:
     os.makedirs(os.path.join(DESTINATION, subset), exist_ok=True)
 
-# ======================
-# PARCOURS DES CLASSES
-# ======================
+#parcours des différentes classes
 
 for class_name in os.listdir(SOURCE_DATASET):
 
@@ -36,21 +34,23 @@ for class_name in os.listdir(SOURCE_DATASET):
 
     images = [
         f for f in os.listdir(class_path)
-        if f.lower().endswith((".jpg",".jpeg",".png"))
+        if f.lower().endswith((".jpg", ".jpeg", ".png"))
     ]
 
     if len(images) == 0:
         continue
 
-    # Train = 70%
+    #split train autre
+
     train_imgs, temp_imgs = train_test_split(
         images,
-        test_size=(1-TRAIN_RATIO),
+        test_size=(1 - TRAIN_RATIO),
         random_state=SEED,
         shuffle=True
     )
 
-    # Validation/Test = 15/15
+    #split val test
+
     val_imgs, test_imgs = train_test_split(
         temp_imgs,
         test_size=0.5,
@@ -64,28 +64,24 @@ for class_name in os.listdir(SOURCE_DATASET):
         "test": test_imgs
     }
 
+    #copie des fichiers
+
     for split_name, split_files in splits.items():
 
-        output_class = os.path.join(
-            DESTINATION,
-            split_name,
-            class_name
-        )
-
+        output_class = os.path.join(DESTINATION, split_name, class_name)
         os.makedirs(output_class, exist_ok=True)
 
         for file in split_files:
             src = os.path.join(class_path, file)
             dst = os.path.join(output_class, file)
-
             shutil.copy2(src, dst)
 
     print(
-        f"{class_name} : "
+        f"{class_name} → "
         f"Train={len(train_imgs)} | "
-        f"Validation={len(val_imgs)} | "
+        f"Val={len(val_imgs)} | "
         f"Test={len(test_imgs)}"
     )
 
 print("\nSéparation terminée.")
-print(f"Données enregistrées dans : {DESTINATION}")
+print(f"Dataset prêt dans : {DESTINATION}")
